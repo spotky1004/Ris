3705d8ba99cf72bc8f7d99379734462e4a691143
Spotky1004 <spotky1004@gmail.com>
2022-10-25 14:30:36 +0900

    Update Item

--- dist/game/core/Item.js
+++ dist/game/core/Item.js
@@ -1,27 +1,32 @@
 export default class Item {
     constructor(options) {
-        var _a, _b, _c, _d;
+        var _a, _b, _c, _d, _e;
         this.name = options.name;
         this.unlockedDefault = (_a = options.unlockedDefault) !== null && _a !== void 0 ? _a : true;
         this.madeFrom = options.madeFrom;
         this.on = options.on;
+        this.timing = options.timing;
         this.chargeOptions = (_b = options.chargeOptions) !== null && _b !== void 0 ? _b : null;
         this.destroyOnEmit = (_c = options.destroyOnEmit) !== null && _c !== void 0 ? _c : false;
         this.onEmit = options.onEmit;
         let tier = (_d = options.tier) !== null && _d !== void 0 ? _d : 1;
-        if (Array.isArray(this.madeFrom)) {
+        let cost = (_e = options.cost) !== null && _e !== void 0 ? _e : 0;
+        if (this.madeFrom.length > 0) {
             tier = Math.max(...this.madeFrom.map(item => item.tier)) + 1;
+            cost = this.madeFrom.reduce((a, b) => a + b.cost, 0);
         }
         this.tier = tier;
+        this.cost = cost;
     }
 }
 const item = new Item({
     name: "Attack Debugger",
     on: "attacked",
+    timing: "after",
     onEmit: ({ game, owner, data }) => {
         game.sender.send(`${data.from.memberName} attacked ${owner.memberName} for ${data.damage} damage!`);
         const players = game.board.getAllPlaceables("Player");
-        console.log(players.includes(owner) ? "The owner is on the board" : "The owner isn't on the board?");
+        console.log(players.includes(owner) ? "The owner is on the board" : "The owner isn't on the board..?");
         const walls = game.board.getAllPlaceables(/^wall/g);
         console.log(walls);
     },
@@ -32,6 +37,7 @@ const item = new Item({
     },
     destroyOnEmit: false,
     tier: 5,
-    madeFrom: 32
+    madeFrom: [],
+    cost: 32
 });
 console.log(item);
--- src/game/core/Item.ts
+++ src/game/core/Item.ts
@@ -3,14 +3,23 @@ import type { TickManagerOptions } from "../etc/TickManager.js";
 import type Player from "../placeables/Player.js";
 
 type ItemActivateEventNames =
-  "used" | "always" |
-  "move" | "kill" | "myTurnStart" | "myTurnEnd" |
+  "used" | "always" | "none" |
+  "move" | "otherPlayerMove" | "kill" | "myTurnStart" | "myTurnEnd" |
   "playerTurnStart" | "playerTurnEnd" | "allTurnStart" | "allTurnEnd" |
   "gameEnd" | "death" | "attack" | "attacked";
-type ItemActivateEventData = {
-  "used": {};
-  "always": {};
+type ItemActivateEventTypes<T> = {
+  [K in ItemActivateEventNames]: T;
+};
+
+interface ItemActivateEventData extends ItemActivateEventTypes<{}> {
+  "always": ItemActivateEventData[Exclude<ItemActivateEventNames, "always">];
+  "none": {};
   "move": {
+    prevPos: [x: number, y: number];
+    curPos: [x: number, y: number];
+  };
+  "otherPlayerMove": {
+    player: Player;
     prevPos: [x: number, y: number];
     curPos: [x: number, y: number];
   };
@@ -45,35 +54,49 @@ type ItemActivateEventData = {
     damage: number;
     from: Player;
   }
-};
+}
+interface ItemActivateEventReturn extends ItemActivateEventTypes<any>  {
+  /** true -> ignore destroyOnEmit */
+  "used": boolean;
+  /** * */
+  "always": any;
+  /** true -> cancel move */
+  "otherPlayerMove": boolean;
+}
 
 interface ItemActivateCallbackArg<T extends ItemActivateEventNames> {
   game: Game;
   owner: Player;
+  event: T;
   data: ItemActivateEventData[T];
 }
-/** Return true to cancel default */
-type ItemActivateCallback<T extends ItemActivateEventNames> = (arg: ItemActivateCallbackArg<T>) => boolean | void;
+type ItemActivateCallback<T extends ItemActivateEventNames> = T extends "always" ?
+  ((arg: ItemActivateCallbackArg<any>) => any) :
+  ((arg: ItemActivateCallbackArg<T>) => ItemActivateEventReturn[T] | void);
 
 interface ItemOptions<T extends ItemActivateEventNames> {
   name: string;
   /** default: true */
   unlockedDefault?: boolean;
-  madeFrom: null | number | Item<any>[];
+  cost?: number;
+  madeFrom: Item[];
   tier?: number;
   on: T;
+  timing: "before" | "after";
   chargeOptions?: TickManagerOptions;
   /** default: false */
   destroyOnEmit?: boolean;
   onEmit: ItemActivateCallback<T>;
 }
 
-export default class Item<T extends ItemActivateEventNames> {
+export default class Item<T extends ItemActivateEventNames = "none"> {
   readonly name: string;
   readonly unlockedDefault: boolean;
-  readonly madeFrom: null | number | Item<any>[];
+  readonly madeFrom: null | number | Item[];
+  cost: number;
   readonly tier: number;
   readonly on: T;
+  readonly timing: "before" | "after";
   readonly chargeOptions: null | TickManagerOptions;
   readonly destroyOnEmit: boolean;
   readonly onEmit: ItemActivateCallback<T>;
@@ -83,26 +106,32 @@ export default class Item<T extends ItemActivateEventNames> {
     this.unlockedDefault = options.unlockedDefault ?? true;
     this.madeFrom = options.madeFrom;
     this.on = options.on;
+    this.timing = options.timing;
     this.chargeOptions = options.chargeOptions ?? null;
     this.destroyOnEmit = options.destroyOnEmit ?? false;
     this.onEmit = options.onEmit;
 
     let tier = options.tier ?? 1;
-    if (Array.isArray(this.madeFrom)) {
+    let cost: number = options.cost ?? 0;
+    if (this.madeFrom.length > 0) {
       tier = Math.max(...this.madeFrom.map(item => item.tier)) + 1;
+      cost = this.madeFrom.reduce((a, b) => a + b.cost, 0);
     }
     this.tier = tier;
+    this.cost = cost;
   }
 }
 
 const item = new Item({
   name: "Attack Debugger",
   on: "attacked",
+  timing: "after",
+
   onEmit: ({ game, owner, data }) => {
     game.sender.send(`${data.from.memberName} attacked ${owner.memberName} for ${data.damage} damage!`);
 
     const players = game.board.getAllPlaceables("Player");
-    console.log(players.includes(owner) ? "The owner is on the board" : "The owner isn't on the board?");
+    console.log(players.includes(owner) ? "The owner is on the board" : "The owner isn't on the board..?");
 
     const walls = game.board.getAllPlaceables(/^wall/g);
     console.log(walls);
@@ -114,6 +143,7 @@ const item = new Item({
   },
   destroyOnEmit: false,
   tier: 5,
-  madeFrom: 32
+  madeFrom: [],
+  cost: 32
 });
 console.log(item);
