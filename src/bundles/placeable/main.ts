f656e32508906175df37deac50c83a8d34ee692e
Spotky1004 <spotky1004@gmail.com>
2022-10-28 09:09:06 +0900

    Remove type import

--- src/bundles/placeable/main.ts
+++ src/bundles/placeable/main.ts
@@ -1,2 +1,2 @@
-export { default as Player, PlayerOptions } from "../../data/placeables/main/Player.js";
-export { default as Wall, WallOptions } from "../../data/placeables/main/Wall.js";
+export { default as Player } from "../../data/placeables/main/Player.js";
+export { default as Wall } from "../../data/placeables/main/Wall.js";
