a747e407c7209da8850a0f23ac1a1d7d60aa407d
Spotky1004 <spotky1004@gmail.com>
2022-10-29 21:40:29 +0900

    Fix commands bundle path

--- dist/bundles/commands.js
+++ dist/bundles/commands.js
@@ -3,6 +3,6 @@ import readModules from "../util/readModules.js";
 import getPath from "../util/getPath.js";
 const { __dirname } = getPath(import.meta.url);
 const commands = new Map(Object.entries(await readModules({
-    dirname: path.join(__dirname, "../command/data")
+    dirname: path.join(__dirname, "../data/commands/main")
 })).map(([key, command]) => [key, command.default]));
 export default commands;
--- src/bundles/commands.ts
+++ src/bundles/commands.ts
@@ -6,7 +6,7 @@ import type { CommandData } from "../data/commands/essentials.js";
 const { __dirname } = getPath(import.meta.url);
 
 const commands = new Map(Object.entries(await readModules({
-  dirname: path.join(__dirname, "../command/data")
+  dirname: path.join(__dirname, "../data/commands/main")
 })).map(([key, command]) => [key, command.default])) as Map<string, CommandData>;
 
 export default commands;
