9ff2137ed5a9bd2b51e183537dbf5bbecc3eb08c
Spotky1004 <spotky1004@gmail.com>
2022-10-29 21:40:46 +0900

    Command /startgame recives user

--- dist/data/commands/main/startgame.js
+++ dist/data/commands/main/startgame.js
@@ -1,14 +1,49 @@
 import { createCommand, slashUtil } from "../essentials.js";
-import { AttachmentBuilder } from "discord.js";
+import Discord from "discord.js";
 import placeable from "../../../bundles/placeable.js";
 const command = createCommand("startgame");
+command.slashCommand
+    .addUserOption(option => option
+    .setName("player1")
+    .setDescription("wa")
+    .setRequired(true))
+    .addUserOption(option => option
+    .setName("player2")
+    .setDescription("!")
+    .setRequired(true))
+    .addUserOption(option => option
+    .setName("player3")
+    .setDescription("sans"))
+    .addUserOption(option => option
+    .setName("player4")
+    .setDescription("!"));
 command.handler = async ({ gameManager, guild, channel, interaction }) => {
     if (!interaction.member) {
         await slashUtil.reply(interaction, "Didn't found member...");
         return;
     }
-    const member = await guild.members.fetch(interaction.user.id);
-    const result = gameManager.createGame([member], channel);
+    const playerPlaces = [
+        interaction.options.getMember("player1"),
+        interaction.options.getMember("player2"),
+        interaction.options.getMember("player3"),
+        interaction.options.getMember("player4")
+    ];
+    const guildMemberPlaces = [];
+    for (let i = 0; i < playerPlaces.length; i++) {
+        const player = playerPlaces[i];
+        if (player === null) {
+            guildMemberPlaces.push(null);
+        }
+        else if ("user" in player) {
+            guildMemberPlaces.push(await guild.members.fetch(player.id));
+        }
+        else {
+            // TODO: fix this
+            guildMemberPlaces.push(null);
+        }
+    }
+    const guildMembers = guildMemberPlaces.filter(v => v);
+    const result = gameManager.createGame(guildMembers, channel);
     if (!result) {
         await slashUtil.reply(interaction, "The game is aleady running!");
         return;
@@ -19,17 +54,24 @@ command.handler = async ({ gameManager, guild, channel, interaction }) => {
         return;
     }
     try {
-        new placeable.main.Player({
-            game,
-            x: 3, y: 3,
-            status: {
-                maxHp: 10,
-                baseAtk: 2.5,
-                baseDef: 0
-            },
-            memberId: interaction.user.id,
-            memberName: member.displayName
-        });
+        for (let i = 0; i < 4; i++) {
+            const guildMember = guildMemberPlaces[i];
+            if (!guildMember)
+                continue;
+            const x = i % 2 ? 5 : 1;
+            const y = i < 2 ? 1 : 5;
+            new placeable.main.Player({
+                game,
+                x, y,
+                status: {
+                    maxHp: 10,
+                    baseAtk: 2.5,
+                    baseDef: 0
+                },
+                memberId: guildMember.id,
+                memberName: guildMember.displayName
+            });
+        }
     }
     catch (_a) {
         await slashUtil.reply(interaction, "Unexpedted error occured...");
@@ -37,7 +79,7 @@ command.handler = async ({ gameManager, guild, channel, interaction }) => {
     }
     const canvas = game.board.canvas;
     void canvas.render();
-    const attachment = new AttachmentBuilder(canvas.toBuffer(), {
+    const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), {
         name: "board.png"
     });
     await slashUtil.reply(interaction, {
