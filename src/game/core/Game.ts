f2c2b7ab421f6825aed3aec66278873f9a7e58ed
Spotky1004 <spotky1004@gmail.com>
2022-10-30 14:59:27 +0900

    Implement turn

--- src/game/core/Game.ts
+++ src/game/core/Game.ts
@@ -13,11 +13,39 @@ export default class Game {
   messageSender: MessageSender;
   board: Board;
   players: PlayerData[];
+  allTurnCount: number;
+  playerTurunCount: number;
 
   constructor(options: GameOptions) {
     this.messageSender = new MessageSender(this, options.discordChannel);
     this.board = new Board(this, options.board);
-
     this.players = options.players;
+    this.allTurnCount = 0;
+    this.playerTurunCount = 0;
+  }
+
+  getAlivePlayerCount() {
+    return this.players.filter(p => !p.defeated).length;
+  }
+
+  getTurnPlayer() {
+    let playerAcc = 0;
+    for (let i = 0; i < this.players.length; i++) {
+      const player = this.players[i];
+      if (playerAcc === this.playerTurunCount) {
+        return player;
+      }
+      if (!player.defeated) playerAcc++;
+    }
+    return null;
+  }
+
+  addPlayerTurn() {
+    this.playerTurunCount++;
+    if (this.playerTurunCount === this.getAlivePlayerCount()) {
+      this.playerTurunCount = 0;
+      this.allTurnCount++;
+    }
+    return this.playerTurunCount;
   }
 }
