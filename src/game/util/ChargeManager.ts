ad40fdb388fcb02f120891e714983fa9dd8b9f20
Spotky1004 <spotky1004@gmail.com>
2022-11-07 12:09:21 +0900

    Better ChargeManager.tick()

--- src/game/util/ChargeManager.ts
+++ src/game/util/ChargeManager.ts
@@ -1,11 +1,13 @@
-import TickManager, { TickManagerOptions } from "./TickManager.js";
+import TickManager, { TickManagerOptions, TickTypes } from "./TickManager.js";
 
 export default class ChargeManager extends TickManager {
   constructor(options: TickManagerOptions) {
     super(options);
   }
 
-  tick() {
+  tick(type: TickTypes) {
+    if (type !== this.type) return false;
+    
     this.timeLeft--;
     if (this.timeLeft <= 0) return true;
     return false;
