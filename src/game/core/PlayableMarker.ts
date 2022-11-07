fc9da6eabfff6f29ed20562487996a252d724dc3
Spotky1004 <spotky1004@gmail.com>
2022-11-07 11:59:08 +0900

    Create PlayableMarker.buyItem(), mergeItems()

--- src/game/core/PlayableMarker.ts
+++ src/game/core/PlayableMarker.ts
@@ -1,5 +1,7 @@
 import PlaceableBase, { PlaceableBaseOptions } from "./PlaceableBase.js";
 import type Player from "./Player.js";
+import type Item from "./Item.js";
+import type WorkingItem from "./WorkingItem.js";
 
 export interface PlayerMoveReturn {
   moveSuccess: boolean;
@@ -30,6 +32,41 @@ export default class PlayableMarker extends PlaceableBase {
 
   look(x: number, y: number) {
     this.looking = [Math.sign(x) as -1 | 0 | 1, Math.sign(y) as -1 | 0 | 1];
+  }
+
+  buyItem(item: Item) {
+    const cost = item.cost;
+    if (cost > this.playerData.money) return false;
+    this.playerData.money -= cost;
+    this.addItem(item);
+    return true;
+  }
+
+  mergeItems(...items: Item[]) {
+    const itemToFind = items.map(item => item.name);
+    let workingItems: WorkingItem[] = [];
+    loop: while (itemToFind.length > 0) {
+      for (const workingItem of this.items) {
+        if (workingItems.includes(workingItem)) {
+          continue;
+        }
+        if (itemToFind.includes(workingItem.data.name)) {
+          workingItems.push(workingItem);
+          const idxToSplice = itemToFind.findIndex(name => name === workingItem.data.name);
+          itemToFind.splice(idxToSplice, 1);
+          continue loop;
+        }
+      }
+      return false;
+    }
+    const mergeResult = this.game.gameManager.itemManager.tryMerge(...workingItems.map(i => i.data));
+    if (mergeResult === null) return false;
+    for (const workingItem of workingItems) {
+      const idxToSplice = workingItems.findIndex(i => i === workingItem);
+      workingItems.splice(idxToSplice, 1);
+    }
+    this.addItem(mergeResult);
+    return true;
   }
 
   death() {
