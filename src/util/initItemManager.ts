56e803ea6a611f9bcd138a4cfc23b091c6b19427
Spotky1004 <spotky1004@gmail.com>
2022-11-09 12:44:14 +0900

    Use itemArr in initItemManager

--- src/util/initItemManager.ts
+++ src/util/initItemManager.ts
@@ -1,20 +1,9 @@
-import itemBundle from "../bundles/item.js";
-import type Item from "../game/core/Item.js";
+import itemArr from "../bundles/itemArr.js";
 import type GameManager from "../game/core/GameManager.js";
-
-export const items: Item[] = [];
-let section: keyof typeof itemBundle;
-for (section in itemBundle) {
-  const itemSection = itemBundle[section] as { [K in string]: Item };
-  for (const itemName in itemSection) {
-    const item = itemSection[itemName];
-    items.push(item);
-  }
-}
 
 export default function initItemManager(gameManager: GameManager) {
   const itemManager = gameManager.itemManager;
-  for (const item of items) {
+  for (const item of itemArr) {
     itemManager.addItem(item);
   }
 }
