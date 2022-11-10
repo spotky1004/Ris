ad52e93eb8c3cf7f1c21b54dbc587bd5223222c7
Spotky1004 <spotky1004@gmail.com>
2022-11-10 21:39:05 +0900

    Better Item info

--- src/game/core/Item.ts
+++ src/game/core/Item.ts
@@ -101,25 +101,19 @@ export default class Item<T extends GameEventNames = any> {
       content += `   < **${this.name}** > **${this.getValueString()}**\n\n`;
       if (this.lore) {
         content += `• ${messages.item["lore"]}\n`;
-        content += `\`\`\`\n`;
-        content += `- ${this.lore.replace(/\n/, "\n  ")}\n`;
-        content += `\`\`\`\n`;
+        content += `- ${this.lore.replace(/\n/, "\n  ")}\n\n`;
       }
       content += `• ${messages.item["description"]}\n`;
-      content += `\`\`\`\n`;
-      content += `- ${this.effectDescription.replace(/\n/, "\n  ")}\n`;
-      content += `\`\`\`\n`;
+      content += `${this.effectDescription.replace(/\n/, "\n  ")}\n\n`;
       if (this.paramDescription.length > 0) {
         content += `• ${messages.item["param"]}\n`;
-        content += `\`\`\`\n`;
         for (let i = 0; i < this.paramDescription.length; i++) {
           const desc = this.paramDescription[i];
           content += `${(i+1)}. ${desc.replace(/\n/, "\n   ")}`;
           content += "\n";
         }
-        content = content.trim();
-        content += `\`\`\`\n`;
       }
+      content = content.trim();
       content += `+${"-".repeat(W - 2)}+`;
     } else {
       content += `+${"-".repeat(W - 2)}+\n`;
